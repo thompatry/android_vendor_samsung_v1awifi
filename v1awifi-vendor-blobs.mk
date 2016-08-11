8e162e76b365ba363f4836b8fb7b707c6c84dd54
Thomas Patry <thompatry@gmail.com>
2016-08-10 21:27:29 -0500

    Fix derp
    Change-Id: I8fcb2766772dc07f3cd3b1ca4aedb7ffaa1f4667

--- v1awifi-vendor-blobs.mk
+++ v1awifi-vendor-blobs.mk
@@ -122,8 +122,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/v1awifi/proprietary/lib/hw/camera.universal5420.so:system/lib/hw/camera.universal5420.so \
     vendor/samsung/v1awifi/proprietary/lib/libexynoscamera.so:system/lib/libexynoscamera.so \
     vendor/samsung/v1awifi/proprietary/lib/libliveframework.so:system/lib/libliveframework.so \
-    vendor/samsung/v1awifi/proprietary/lib/libvdis.so:system/lib/libvdis.so
-    vendor/samsung/v1awifi/proprietary/lib/libmor.so:system/lib/libmor.so \
+    vendor/samsung/v1awifi/proprietary/lib/libvdis.so:system/lib/libvdis.so \
+    vendor/samsung/v1awifi/proprietary/lib/libmor.so:system/lib/libmor.so
 
 # Sensors
 PRODUCT_COPY_FILES += \
